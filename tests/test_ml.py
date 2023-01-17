a3dace135e08e295d27f7e343f1844a432f5b26d
w11wo <wilsonwong961@gmail.com>
2023-01-17 11:26:57 +1100

    Updated Tests to New Scripts

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -30,7 +30,7 @@ from scripts.aac_to_wav import parse_args, convert_to_wav
 
 def test_convert_to_wav(datadir):
     datadir = str(datadir)
-    parser = parse_args([datadir, "-c", "2", "-r", "24_000"])
+    parser = parse_args(["--input_dir", datadir, "-c", "2", "-r", "24_000"])
     assert parser.input_dir == datadir
     assert parser.channel == 2
     assert parser.rate == 24_000
