899dd284d0b2dbe716be06f7daca37ee5e7d0eca
w11wo <wilsonwong961@gmail.com>
2023-01-24 10:20:35 +0700

    Removed fp16; Reduced wav2vec2 Batch Size

--- speechline/ml/transcriber.py
+++ speechline/ml/transcriber.py
@@ -136,7 +136,6 @@ class Wav2Vec2Transcriber(AudioModule):
         args = TrainingArguments(
             output_dir="./",
             per_device_eval_batch_size=batch_size,
-            fp16=torch.cuda.is_available(),
         )
 
         trainer = Trainer(
