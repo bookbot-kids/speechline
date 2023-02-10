484a194bb849cdadf025286488c882e489b796b3
w11wo <wilsonwong961@gmail.com>
2023-02-10 15:13:11 +0700

    Updated Test Suites for Batch PER

--- tests/test_per.py
+++ tests/test_per.py
@@ -26,113 +26,118 @@ def test_phoneme_error_rate():
     per = PhonemeErrorRate(lexicon)
     words = ["hello", "guy"]
 
-    assert per(words, prediction=["h", "a", "l", "l", "o", "g", "a", "i"]) == 0.0
-    assert per(words, prediction=["h", "a", "l", "l", "X", "g", "a", "i"]) == 0.125
+    assert per([words], [["h", "a", "l", "l", "o", "g", "a", "i"]]) == 0.0
+    assert per([words], [["h", "a", "l", "l", "X", "g", "a", "i"]]) == 0.125
+    assert per([words], [["h", "a", "l", "l", "X", "X", "X", "g", "a", "i"]]) == 0.375
+    assert per([words], [["h", "a", "X", "g", "a", "i"]]) == 0.375
+    assert per([words], [["h", "a", "l", "l", "X", "o", "g", "a", "i"]]) == 0.125
     assert (
-        per(words, prediction=["h", "a", "l", "l", "X", "X", "X", "g", "a", "i"])
+        per(
+            [words],
+            [["h", "a", "l", "l", "X", "X", "X", "o", "g", "a", "i"]],
+        )
         == 0.375
     )
-    assert per(words, prediction=["h", "a", "X", "g", "a", "i"]) == 0.375
-    assert per(words, prediction=["h", "a", "l", "l", "X", "o", "g", "a", "i"]) == 0.125
+    assert per([words], [["h", "a", "l", "l", "g", "a", "i"]]) == 0.125
+    assert per([words], [["h", "a", "l", "g", "a", "i"]]) == 0.25
+    assert per([words], [["h", "e", "l", "l", "o", "g", "a", "i"]]) == 0.0
     assert (
-        per(words, prediction=["h", "a", "l", "l", "X", "X", "X", "o", "g", "a", "i"])
+        per(
+            [words],
+            [["h", "X", "e", "X", "X", "l", "l", "o", "g", "a", "i"]],
+        )
         == 0.375
     )
-    assert per(words, prediction=["h", "a", "l", "l", "g", "a", "i"]) == 0.125
-    assert per(words, prediction=["h", "a", "l", "g", "a", "i"]) == 0.25
-    assert per(words, prediction=["h", "e", "l", "l", "o", "g", "a", "i"]) == 0.0
-    assert (
-        per(words, prediction=["h", "X", "e", "X", "X", "l", "l", "o", "g", "a", "i"])
-        == 0.375
-    )
-    assert (
-        per(words, prediction=["h", "e", "X", "X", "l", "l", "o", "g", "a", "i"])
-        == 0.25
-    )
-    assert per(words, prediction=["h", "e", "o", "g", "a", "i"]) == 0.25
-    assert per(words, prediction=["h", "e", "X", "l", "o", "g", "a", "i"]) == 0.125
+    assert per([words], [["h", "e", "X", "X", "l", "l", "o", "g", "a", "i"]]) == 0.25
+    assert per([words], [["h", "e", "o", "g", "a", "i"]]) == 0.25
+    assert per([words], [["h", "e", "X", "l", "o", "g", "a", "i"]]) == 0.125
 
     words = ["hello", "guy", "hello"]
 
     assert (
         per(
-            words,
-            prediction=[
-                "h",
-                "a",
-                "l",
-                "l",
-                "o",
-                "g",
-                "a",
-                "i",
-                "h",
-                "e",
-                "l",
-                "l",
-                "o",
+            [words],
+            [
+                [
+                    "h",
+                    "a",
+                    "l",
+                    "l",
+                    "o",
+                    "g",
+                    "a",
+                    "i",
+                    "h",
+                    "e",
+                    "l",
+                    "l",
+                    "o",
+                ]
             ],
         )
         == 0.0
     )
     assert (
-        per(words, prediction=["h", "a", "l", "l", "o", "g", "h", "e", "o"])
+        per([words], [["h", "a", "l", "l", "o", "g", "h", "e", "o"]])
         == 0.3076923076923077
     )
     assert (
         per(
-            words,
-            prediction=[
-                "h",
-                "a",
-                "l",
-                "l",
-                "X",
-                "o",
-                "g",
-                "a",
-                "i",
-                "X",
-                "h",
-                "X",
-                "e",
-                "X",
-                "l",
-                "l",
-                "o",
+            [words],
+            [
+                [
+                    "h",
+                    "a",
+                    "l",
+                    "l",
+                    "X",
+                    "o",
+                    "g",
+                    "a",
+                    "i",
+                    "X",
+                    "h",
+                    "X",
+                    "e",
+                    "X",
+                    "l",
+                    "l",
+                    "o",
+                ]
             ],
         )
         == 0.3076923076923077
     )
     assert (
         per(
-            words,
-            prediction=["h", "a", "X", "o", "g", "X", "i", "h", "e", "l", "l", "X"],
+            [words],
+            [["h", "a", "X", "o", "g", "X", "i", "h", "e", "l", "l", "X"]],
         )
         == 0.3076923076923077
     )
     assert (
-        per(words, prediction=["g", "a", "i", "h", "e", "l", "l", "o"])
-        == 0.38461538461538464
+        per([words], [["g", "a", "i", "h", "e", "l", "l", "o"]]) == 0.38461538461538464
     )
     assert (
         per(
-            words,
-            prediction=[
-                "h",
-                "a",
-                "l",
-                "X",
-                "X",
-                "X",
-                "X",
-                "a",
-                "i",
-                "h",
-                "X",
-                "l",
-                "l",
-                "o",
+            [words],
+            [
+                [
+                    "h",
+                    "a",
+                    "l",
+                    "X",
+                    "X",
+                    "X",
+                    "X",
+                    "a",
+                    "i",
+                    "h",
+                    "X",
+                    "l",
+                    "l",
+                    "o",
+                ]
             ],
         )
         == 0.38461538461538464
